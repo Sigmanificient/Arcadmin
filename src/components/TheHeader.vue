e5b9f30e872bfd3614a834166d5c17342ab4017e
sigmanificient <edhyjox@gmail.com>
2022-07-27 23:54:44 +0200

    :sparkles: Added header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+	<header class="header">
+		<h1>Panel</h1>
+	</header>
+</template>
+
+<style lang="scss">
+.header {
+	position: absolute;
+	z-index: 1;
+
+	left: 45%;
+
+	bottom: 0;
+	height: 48px;
+
+	background: #1a1f25;
+	color: white;
+
+	h1 {
+		margin-block: 0;
+		font-size: 16px;
+		font-family: "Inter", sans-serif;
+	}
+}
+</style>
